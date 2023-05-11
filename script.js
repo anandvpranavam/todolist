344386a4e04d9a86af83f139573ac08607942721
Anand Venugopalapanicker <anandvpranavam@gmail.com>
2023-05-10 20:43:53 -0400

    rewrited sript.js

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 const input = document.getElementById("input")
-console.log("hello how are you?", input)
 const addButton = document.getElementById("addButton")
-console.log(addButton)
-const list = document.getElementById("listElements")
-console.log("Hello", list)
+const list = document.getElementById("list")
 
-// function handleClick() {
-//     console.log("clicked me", input.value);
-//     addListItem(input.value);
-// }
-function addListItem(itemValue) {
-    if(itemValue !== ""){
-    const li = document.createElement("li");
+function addListItem(itemValue){
+    if (itemValue !== ""){
+        const li = document.createElement(li)
     li.innerText = itemValue;
-    list.appendChild(li);
-    }
+    list.appendChild(li)
+    } 
 }
-// addButton.addEventListener("click", handleClick)
+
 
 const myForm = document.getElementById("myForm")
 myForm.addEventListener("submit", handleSubmit)
 
 function handleSubmit(event){
     event.preventDefault();
-    console.log("hey", event);
-    addListItem(input.value);
+    addListItem(input.value)
     input.value = "";
 }
