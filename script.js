4276f5facb4e8b4105daf7ecda678ea30da84b3a
Anand Venugopalapanicker <anandvpranavam@gmail.com>
2023-05-24 21:25:43 -0400

    added Done and delete button, checked isDone true/false, added strikethrough style if done, added splice method for deleting item

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
 const form = document.getElementById("myForm")
 const todoArray = []
 
-class Todo{
-    constructor(textItem, isDone){
-        this.textItem = textItem;
-        this.isDone = isDone;
+
+class Todo {
+    constructor(text, isDone) {
+        this.text = text
+        this.isDone = isDone
     }
 }
 
-function handleSubmit(event){
-    event.preventDefault();
-    const input = document.getElementById("input")
-    const todoItem = new Todo(input.value, false)
-    todoArray.push(todoItem)
-    console.log(todoArray)
-    displayTodoArray()
-    input.value = "";
+function handleDone(i) {
+    console.log(i)
+    todoArray[i].isDone = true;
+    displayTodo()
+}
+function handleDelete(i){
+    console.log(i)
+    todoArray.splice(i, 1)
+    displayTodo()
 
-} 
-
-function displayTodoArray(){
-     const mainDiv = document.getElementById("myDiv")
-     mainDiv.innerHTML="";
-     todoArray.forEach(todo =>{
+}
+function displayTodo() {
+    const mainDIv = document.getElementById("myDiv")
+    console.log(mainDIv)
+    mainDIv.innerHTML = "";
+    todoArray.forEach((todo, index) => {
         const subDiv = document.createElement("div")
-        const todoString = JSON.stringify(todo)
-        subDiv.innerText = todoString;
-        mainDiv.appendChild(subDiv);
-     })
+        const todoString = todo.text
+        subDiv.innerText = todoString
+        
+        if (todo.isDone === true) {
+            subDiv.style = "text-decoration:line-through"
+        }
+        else {
+            const doneButton = document.createElement("button")
+            doneButton.innerText = "Done"
+            subDiv.appendChild(doneButton)
+            doneButton.addEventListener("click", () => handleDone(index))
+        }
+        const deleteButton = document.createElement("button")
+        deleteButton.innerText = "Delete"
+        subDiv.appendChild(deleteButton)
+        deleteButton.addEventListener("click", ()=>handleDelete(index))
+        console.log(todoString)
+        mainDIv.appendChild(subDiv)
+
+    })
 }
 
+function handleSubmit(event) {
+
+    event.preventDefault()
+    const input = document.getElementById("input")
+    const textInput = new Todo(input.value, false)
+    todoArray.push(textInput)
+    console.log(textInput)
+    displayTodo()
+    input.value = ""
+}
 form.addEventListener("submit", handleSubmit)
