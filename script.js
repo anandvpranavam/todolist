3683b26ea1e276d2f09c312bc1b436372f87efa4
Anand Venugopalapanicker <anandvpranavam@gmail.com>
2023-05-26 21:58:26 -0400

    added function for storing array value to local storage and retrieve value from local storage; stringify array value to store; parse stringyfied value to restore to array

--- script.js
+++ script.js
@@ -1,61 +1,71 @@
 const form = document.getElementById("myForm")
-const todoArray = []
+const array = getFromLocalStorage()
+displayTodo()
 
-
-class Todo {
-    constructor(text, isDone) {
+class Todo{
+    constructor (text, isDone){
         this.text = text
         this.isDone = isDone
     }
 }
-
-function handleDone(i) {
+function storeToLocalStorage(){
+    localStorage.setItem("todos", JSON.stringify(array))
+}
+function getFromLocalStorage(){
+    const todosString = localStorage.getItem("todos")
+    // if(todosString){
+    //     return JSON.parse(todosString)
+    // }
+    // else{
+    //     return []
+    // }
+    return todosString ? JSON.parse(todosString) : [];
+}
+function handleDone(i){
     console.log(i)
-    todoArray[i].isDone = true;
+    array[i].isDone = true
     displayTodo()
 }
 function handleDelete(i){
-    console.log(i)
-    todoArray.splice(i, 1)
+    array.splice(i, 1)
     displayTodo()
-
 }
-function displayTodo() {
+function displayTodo(){
     const mainDIv = document.getElementById("myDiv")
-    console.log(mainDIv)
-    mainDIv.innerHTML = "";
-    todoArray.forEach((todo, index) => {
+    mainDIv.innerHTML=""
+    array.forEach((todo,index) =>{
         const subDiv = document.createElement("div")
-        const todoString = todo.text
-        subDiv.innerText = todoString
+        console.log(subDiv)
+        subDiv.innerText = todo.text
+        console.log(todo)
+        mainDIv.appendChild(subDiv)
         
-        if (todo.isDone === true) {
-            subDiv.style = "text-decoration:line-through"
+        // console.log(mainDIv.appendChild(subDiv))
+        
+        if(todo.isDone===true){
+            subDiv.style="text-decoration:line-through"
         }
-        else {
-            const doneButton = document.createElement("button")
-            doneButton.innerText = "Done"
-            subDiv.appendChild(doneButton)
-            doneButton.addEventListener("click", () => handleDone(index))
+        else{
+        const doneButton = document.createElement("button")
+        doneButton.innerText = "Done"
+        subDiv.appendChild(doneButton)
+        doneButton.addEventListener("click", ()=>handleDone(index))
         }
+
         const deleteButton = document.createElement("button")
         deleteButton.innerText = "Delete"
         subDiv.appendChild(deleteButton)
         deleteButton.addEventListener("click", ()=>handleDelete(index))
-        console.log(todoString)
-        mainDIv.appendChild(subDiv)
-
     })
+    storeToLocalStorage()
 }
-
-function handleSubmit(event) {
-
+function handleSubmit(event){
     event.preventDefault()
     const input = document.getElementById("input")
-    const textInput = new Todo(input.value, false)
-    todoArray.push(textInput)
-    console.log(textInput)
+    const inputItem = new Todo (input.value, false)
+    array.push(inputItem)
+    console.log(inputItem)
     displayTodo()
-    input.value = ""
+    input.value=""
 }
 form.addEventListener("submit", handleSubmit)
